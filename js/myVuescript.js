8775f9a79daa8258fd228cea3594bf32812e4422
Alessandro <luisialessandro95@gmail.com>
2021-11-29 00:35:16 +0100

    Correzione: vero MegaArray

--- js/myVuescript.js
+++ js/myVuescript.js
@@ -3,29 +3,6 @@
 const app = new Vue({
     el: '#root',
     data: {
-        itemsArrey: [
-            'img/01.jpg',
-            'img/02.jpg',
-            'img/03.jpg',
-            'img/04.jpg',
-            'img/05.jpg'
-        ],
-        
-        titleArrey: [
-            'Sasso Marconi',
-            'Rocca Rasu',
-            'Formello',
-            'Bassano del Grappa',
-            'Pianarella di Cervia'
-        ],
-        
-        textArrey: [
-            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-            'Lorem ipsum',
-            'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-            'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-        ],
 
         MegaArray: [
             {   
@@ -65,25 +42,24 @@ const app = new Vue({
 
     methods:{
         increment: function(){
-            if (this.courrentImg == this.itemsArrey.length - 1){
+            if (this.courrentImg == this.MegaArray.length - 1){
                 this.courrentImg = 0;
             }else{
                 this.courrentImg++;
             }
-            
         },
 
         decrement: function(){
             if (this.courrentImg == 0){
-                this.courrentImg = this.itemsArrey.length - 1;
+                this.courrentImg = this.MegaArray.length - 1;
             }else{
                 this.courrentImg--;
             }
         },
+
         selectObject: function(i){
             this.courrentImg = i;
         }
-
     }
 });
 
